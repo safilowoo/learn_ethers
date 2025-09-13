8ab8df63bbe7c2251e2b074096a8a2f4f043f66c
Safilo <Safilo@Safilos-MacBook-Pro.local>
2025-09-13 10:46:15 +0800

    add configs

--- src/03_Contract.js
+++ src/03_Contract.js
@@ -1,12 +1,12 @@
 import "dotenv/config";
 import { ethers } from "ethers";
+import configs from "./configs.js";
 
-const provider = new ethers.JsonRpcProvider(process.env.ALCHEMY_MAINNET_URL);
+const provider = new ethers.JsonRpcProvider(process.env.ALCHEMY_SEPOLIA_URL);
 
 // WETH
-const abiWETH =
-  '[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"guy","type":"address"},{"name":"wad","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"src","type":"address"},{"name":"dst","type":"address"},{"name":"wad","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"wad","type":"uint256"}],"name":"withdraw","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"dst","type":"address"},{"name":"wad","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"deposit","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"src","type":"address"},{"indexed":true,"name":"guy","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"src","type":"address"},{"indexed":true,"name":"dst","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"dst","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Deposit","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"src","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Withdrawal","type":"event"}]';
-const addressWETH = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
+const abiWETH = configs.WETH_SEPOLIA_ABI;
+const addressWETH = configs.WETH_SEPOLIA_URL;
 const contractWETH = new ethers.Contract(addressWETH, abiWETH, provider);
 
 // Human-Readable Abi, eg.DAI
@@ -16,7 +16,7 @@ const abiERC20 = [
   "function totalSupply() view returns (uint256)",
   "function balanceOf(address) view returns (uint)",
 ];
-const addressDAI = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+const addressDAI = configs.DAI_SEPOLIA_URL;
 const contractDAI = new ethers.Contract(addressDAI, abiERC20, provider);
 
 const main = async () => {
